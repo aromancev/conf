bd94169e50705b4cb28719b1e00004f9e8852243
Buzov Vyahceslav <buzovv1997@gmail.com>
2021-02-28 15:44:34 +0300

    deploy scripts & env variables

--- deploy/postgres.sh
+++ deploy/postgres.sh
@@ -3,5 +3,12 @@
 ROOT="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )")"
 
 scp -C $ROOT/docker-compose.yml $USER@$IP:~
-ssh $USER@$IP "docker-compose up --no-deps -d"
+ssh $USER@$IP \
+  POSTGRES_HOST="$POSTGRES_HOST" \
+  POSTGRES_PORT="$POSTGRES_PORT" \
+  POSTGRES_USER="$POSTGRES_USER" \
+  POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
+  POSTGRES_DATABASE="$POSTGRES_DATABASE" \
+  "docker-compose up --no-deps -d"
+
 ssh $USER@$IP "docker image prune -f"
