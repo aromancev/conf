031af03ff7fa12094f08aaa6b4253e1ef1f03595
Andrey Romancev <andromancev@gmail.com>
2021-03-11 17:36:48 +0100

    Extend handle up to 64 chars.

--- api/internal/migrations/20210228174503_init.sql
+++ api/internal/migrations/20210228174503_init.sql
@@ -17,7 +17,7 @@ CREATE TABLE idents (
 CREATE TABLE confas (
     id UUID PRIMARY KEY,
     owner UUID NOT NULL,
-    handle VARCHAR(32) NOT NULL,
+    handle VARCHAR(64) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
 -- +goose StatementEnd
