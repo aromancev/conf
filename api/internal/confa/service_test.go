fedab41b7b3ccff30e186a7b02fadb4cd05db060
aromancev <a.romancev@pangeoradar.ru>
2021-03-16 22:41:33 +0300

    refactor according to suggested changes

--- api/internal/confa/service_test.go
+++ api/internal/confa/service_test.go
@@ -14,18 +14,18 @@ func TestCRUD(t *testing.T) {
 
 	ctx := context.Background()
 
-	t.Run("SetGet", func(t *testing.T) {
+	t.Run("Fetch", func(t *testing.T) {
 		t.Parallel()
 
 		pg, done := double.NewDocker(migrations)
 		defer done()
 		crud := NewCRUD(pg, NewSQL())
-		userid := uuid.New()
+		userID := uuid.New()
 		request := Confa{
 			Handle: "test",
 		}
 
-		confa, err := crud.Create(ctx, userid, request)
+		confa, err := crud.Create(ctx, userID, request)
 		require.NoError(t, err)
 		fetchedConfa, err := crud.Fetch(ctx, confa.ID, confa.Owner)
 		require.NoError(t, err)
