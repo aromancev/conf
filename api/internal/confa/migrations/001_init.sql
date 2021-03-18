9616ffd7f15fd23353c31a7f11247020620fda15
Buzov Vyahceslav <buzovv1997@gmail.com>
2021-03-18 22:33:51 +0300

    migrations fix after rebase

--- api/internal/confa/migrations/001_init.sql
+++ api/internal/confa/migrations/001_init.sql
@@ -1,10 +1,19 @@
 CREATE TABLE confas (
     id UUID PRIMARY KEY,
     owner UUID NOT NULL,
-    handle VARCHAR(64) NOT NULL,
+    handle VARCHAR(64) NOT NULL UNIQUE,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE talks (
+    id UUID PRIMARY KEY,
+    confa UUID NOT NULL REFERENCES confas (id) ON DELETE CASCADE,
+    handle VARCHAR(64) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    UNIQUE (confa, handle)
 );
 
 ---- create above / drop below ----
 
 DROP TABLE confas;
+DROP TABLE talks;
