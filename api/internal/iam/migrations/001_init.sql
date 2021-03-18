4674636bc81ddca58dfeb74bf691cb41d31f9fab
Buzov Vyahceslav <buzovv1997@gmail.com>
2021-03-18 22:33:51 +0300

    migrations fix after rebase

--- api/internal/iam/migrations/001_init.sql
+++ api/internal/iam/migrations/001_init.sql
@@ -12,26 +12,9 @@ CREATE TABLE idents (
     UNIQUE (platform, value)
 );
 
-CREATE TABLE confas (
-    id UUID PRIMARY KEY,
-    owner UUID NOT NULL,
-    handle VARCHAR(64) NOT NULL UNIQUE,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL
-);
-
-CREATE TABLE talks (
-    id UUID PRIMARY KEY,
-    confa UUID NOT NULL REFERENCES confas (id) ON DELETE CASCADE,
-    handle VARCHAR(64) NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    UNIQUE (confa, handle)
-);
-
 ---- create above / drop below ----
 
 DROP TABLE users;
 DROP TABLE idents;
-DROP TABLE confas;
-DROP TABLE talks;
 
 DROP TYPE platform;
