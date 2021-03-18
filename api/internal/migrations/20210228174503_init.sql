8a93bffe03906dd78b5d3d96bb627be2d813a13e
Buzov Vyahceslav <buzovv1997@gmail.com>
2021-03-18 21:38:44 +0300

    talk crud implemented

--- api/internal/migrations/20210228174503_init.sql
+++ api/internal/migrations/20210228174503_init.sql
@@ -17,9 +17,18 @@ CREATE TABLE idents (
 CREATE TABLE confas (
     id UUID PRIMARY KEY,
     owner UUID NOT NULL,
-    handle VARCHAR(64) NOT NULL,
+    handle VARCHAR(64) NOT NULL UNIQUE,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
+
+CREATE TABLE talks (
+    id UUID PRIMARY KEY,
+    confa UUID NOT NULL REFERENCES confas (id) ON DELETE CASCADE,
+    handle VARCHAR(64) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    UNIQUE (confa, handle)
+);
+
 -- +goose StatementEnd
 
 -- +goose Down
@@ -27,4 +36,5 @@ CREATE TABLE confas (
 DROP TABLE users;
 DROP TABLE idents;
 DROP TABLE confas;
+DROP TABLE talks;
 -- +goose StatementEnd
