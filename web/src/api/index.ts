1f27893d63779a6c2959272be3aa7e33a9da7ff1
Andrey Romancev <andromancev@gmail.com>
2022-03-01 18:49:04 +0100

    Refactor RTC API. (#93)

--- web/src/api/index.ts
+++ web/src/api/index.ts
@@ -5,7 +5,7 @@ import { EventClient } from "./event"
 import { ConfaMask as SchemaConfaMask, TalkMask as SchemaTalkMask } from "./schema"
 
 export * from "./models"
-export * from "./rtc"
+export * from "./room"
 export * from "./api"
 export type ConfaMask = SchemaConfaMask
 export type TalkMask = SchemaTalkMask
--- web/src/api/room/index.ts
+++ web/src/api/room/index.ts
@@ -1,3 +1,4 @@
 export * from "./buffered"
 export * from "./messages"
 export * from "./peers"
+export * from "./rtc"
