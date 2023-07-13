e5a42fb4756af97da7fbc2eabc5b9725923bf8d7
Andrey Romancev <andromancev@gmail.com>
2023-07-13 20:01:13 +0200

    refactor: add storage and beanstalk configs as consul keys

--- deploy/terraform/prod/consul/main.tf
+++ deploy/terraform/prod/consul/main.tf
@@ -11,3 +11,39 @@ resource "consul_key_prefix" "auth" {
     "public_key"  = tls_private_key.main.public_key_pem
   }
 }
+
+resource "consul_key_prefix" "web" {
+  path_prefix = "web/"
+
+  subkeys = {
+    "host"   = "confa.io"
+    "scheme" = "https"
+  }
+}
+
+resource "consul_key_prefix" "beanstalk_tubes" {
+  path_prefix = "beanstalk/"
+
+  subkeys = {
+    "tubes/send"                   = "sender/send"
+    "tubes/update-avatar"          = "confa/update-avatar"
+    "tubes/start-recording"        = "confa/start-recording"
+    "tubes/stop-recording"         = "confa/stop-recording"
+    "tubes/recording-update"       = "confa/recording-update"
+    "tubes/store-event"            = "rtc/store-event"
+    "tubes/update-recording-track" = "rtc/update-recording-track"
+    "tubes/process-track"          = "avp/process-track"
+  }
+}
+
+resource "consul_key_prefix" "storage" {
+  path_prefix = "storage/"
+
+  subkeys = {
+    "public-url"                    = "/api/storage"
+    "buckets/user-public"           = "user-public"
+    "buckets/user-uploads"          = "user-uploads"
+    "buckets/confa-tracks-internal" = "confa-tracks-internal"
+    "buckets/confa-tracks-public"   = "confa-tracks-public"
+  }
+}
