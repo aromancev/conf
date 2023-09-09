8e08b98725059c8efb589573d5d2b3a3ad06be7f
Andrey Romancev <andromancev@gmail.com>
2023-09-09 13:56:36 +0200

    refactor: restrict media sharing resolutions

--- services/tracker/record/record.go
+++ services/tracker/record/record.go
@@ -135,11 +135,6 @@ func (t *Tracker) writeTrack(ctx context.Context, track *webrtc.TrackRemote, pli
 	objectPath := path.Join(t.roomID.String(), recordID.String())
 	tmpFilePath := path.Join(t.tmpDir, fmt.Sprintf("%s_%s", t.roomID.String(), recordID.String()))
 
-	err := os.MkdirAll(path.Join(t.tmpDir, t.roomID.String()), 0o700)
-	if err != nil {
-		log.Ctx(ctx).Err(err).Msg("Failed to create temporary dir.")
-		return
-	}
 	tmpFile, err := os.Create(tmpFilePath)
 	if err != nil {
 		log.Ctx(ctx).Err(err).Msg("Failed to open temporary file for buffering track.")
@@ -210,18 +205,16 @@ func (t *Tracker) writeTrack(ctx context.Context, track *webrtc.TrackRemote, pli
 		}()
 
 		for {
-			log.Ctx(ctx).Debug().Msg("Reading RTP.")
 			packet, _, err := track.ReadRTP()
 			switch {
 			case errors.Is(err, io.EOF):
-				log.Ctx(ctx).Debug().Msg("Track ended when writing RTP.")
+				log.Ctx(ctx).Debug().Msg("Track ended when reading RTP.")
 				return
 			case err != nil:
 				log.Ctx(ctx).Err(err).Msg("Failed to read RTP.")
 				continue
 			}
 
-			log.Ctx(ctx).Debug().Msg("Writing RTP.")
 			if err := rtpWriter.WriteRTP(packet); err != nil {
 				log.Ctx(ctx).Warn().Msg("Failed to write RTP packet.")
 				continue
